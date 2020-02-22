761ab86d97c1642a53a2f106b758f536c1bf4543
Ashmin Bhandari <abhandar@ramapo.edu>
2020-02-22 14:58:56 -0500

    Initial commit

---
+++ src/components/Landing.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Landing extends React.Component {
+    render() {
+       return(
+           <div className="full-height">
+           <div className="card text-white bg-dark mb-3">
+               <div className="card-header">Header</div>
+               <div className="card-body">
+                   <h5 className="card-title">Dark card title</h5>
+                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of
+                       the card's content.</p>
+               </div>
+           </div>
+           </div>
+       )
+    }
+}
+
+export default Landing;
