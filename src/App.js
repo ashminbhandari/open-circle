10d846af5b31e117b42b5bef3508f3a7bb4a38f0
Ashmin Bhandari <abhandar@ramapo.edu>
2020-02-22 20:08:28 -0500

    React router setup, map page working.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { BrowserRouter as Router,  Switch} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
-import MapPage from './components/MapPage';
-
 
 function App() {
   return (
@@ -15,7 +13,7 @@ function App() {
         </div>
             <Switch>
             <Route path="/" exact component={Landing}/>
-            <Route path="/map" exact component={MapPage}/>
+            <Route path="/" component={Landing}/>
             </Switch>
         </Router>
 
