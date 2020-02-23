5eb516241859040ad23b088c038480d4537dea98
Ashmin Bhandari <abhandar@ramapo.edu>
2020-02-22 22:49:14 -0500

    Geolocation doesn't work without HTTPS, switched it so

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,41 +8,20 @@ import './Landing.css';
 import MapPage from "./MapPage";
 
 
-function getPosition() {
-    // Simple wrapper
-    return new Promise((res, rej) => {
-        navigator.geolocation.getCurrentPosition(res, rej);
-    });
-}
+
 
 class Landing extends React.Component {
     constructor() {
         super();
-        this.state = { checked: false };
+        this.state = { checked: false, userData: ""};
         this.handleChange = this.handleChange.bind(this);
     }
 
 
-    async handleChange(checked){
+    handleChange(checked){
         //validate post request data *
         //if no error proceed to set state else: display error *
         this.setState({ checked: true });
-        var position = await getPosition();  // wait for getPosition to complete
-
-        var userInfo = {'toggleOn': checked, 'latitude':position.coords.latitude,'longitude':position.coords.longitude};
-        try
-        {
-            await axios.post('http://192.168.4.179:5000/database/toggle', userInfo);
-
-        }
-        catch(error)
-        {
-            console.log(error);
-        }
-
-
-
-
     }
     render() {
         if(!this.state.checked) {
@@ -71,8 +50,7 @@ class Landing extends React.Component {
             )
         }
         else {
-            console.log(this.state.checked);
-            return(<MapPage/>);
+            return(<MapPage />);
         }
     }
 
--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import {geolocated} from 'react-geolocated';
+import axios from "axios";
 
 const mapStyles = {
     width: '100%',
@@ -8,15 +9,76 @@ const mapStyles = {
 };
 
 
+
+
 class MapPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude: null,
+            longitude: null
+        };
+
+        this.getLocation = this.getLocation.bind(this);
+        this.getPosition = this.getPosition.bind(this);
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.getPosition);
+        } else {
+            alert("Geolocation is not supported by the browser.")
+        }
+    }
+
+    async getPosition(position) {
+        console.log(position);
+        this.setState(
+            {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }
+        )
+
+        var userInfo = {
+            toggle: true,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+
+        
+
+        try
+        {
+            await axios.post('http://192.168.4.179:5000/database/toggle', userInfo);
+        }
+        catch(error)
+        {
+            console.log(error);
+        }
+
+    }
+
+    componentDidMount() {
+        this.getLocation();
+    }
+
+
+
     render() {
-        return (<div>
+
+        return (
+
+            <div>
                 <Map
                     google={this.props.google}
-                    zoom={8}
+                    zoom={16}
                     style={mapStyles}
-                    initialCenter={{ lat: 47.444, lng: -122.176}}
-                />
+                    zoom={16}
+                    center={{ lat: this.state.latitude, lng: this.state.longitude}}>
+                    <Marker position={{ lat: this.state.latitude, lng: this.state.longitude}} />
+
+                </Map>
             </div>
         );
     }
@@ -25,3 +87,5 @@ class MapPage extends React.Component {
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyCR6T6kFBhC8Q3AWK0ELT2leqsQrIM8fig'
 })(MapPage);
+
+
